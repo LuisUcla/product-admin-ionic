7fae7fdd1e7a1eef4c1bed90fc38a4ae6b42eea1
LuisUcla <caluigi7@gmail.com>
2024-07-09 21:57:44 -0300

    feat: add services of firebase and utils and handelErrors

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, inject } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { User } from '../models/user.model';
+import {AngularFirestore} from '@angular/fire/compat/firestore'
+import {getFirestore, setDoc, doc } from '@angular/fire/firestore'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore) // para ejecutar operaciones en la DB
+
+  // ========== Acceder =====================
+  signIn(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  // ========== crear usuario ===============
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  // ========== Actualizar usuario  ===============
+  updateUser(displayName: string) {
+    return updateProfile(getAuth().currentUser, {displayName})
+  }
+
+  // ================== Base de datos =================
+
+  // setear o guardar en la DB
+  setDocument(path: string, data: any) {
+    return setDoc(doc(getFirestore(), path), data);
+  }
+}
---
+++ src/app/shared/firebaseError.ts
@@ -0,0 +1,4 @@
+export const firebaseError = {
+    "auth/invalid-credential": "Usuario o contrasena incorrecta",
+    "auth/email-already-in-use": "El correo proporcionado ya exist"
+}
