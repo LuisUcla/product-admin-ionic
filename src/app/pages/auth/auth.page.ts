9854444a5fa9c04cee1e9c6bca50bcdbbae3dc11
LuisUcla <caluigi7@gmail.com>
2024-07-09 22:00:18 -0300

    feat: auth and signUp

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from '../../services/firebase.service';
+import { User } from '../../models/user.model';
+import { UtilsService } from 'src/app/services/utils.service';
+import { firebaseError } from 'src/app/shared/firebaseError';
 
 @Component({
   selector: 'app-auth',
@@ -12,14 +16,36 @@ export class AuthPage implements OnInit {
     password: new FormControl('', [Validators.required]),
   })
 
-  constructor() { }
+  firebaseService = inject(FirebaseService)
+  utlisService = inject(UtilsService);
 
   ngOnInit() {
   }
 
-  submit() {
-  
-    console.log(this.form.value)
+ async submit() {
+    const user = this.form.value as User
+    const loading = await this.utlisService.presentLoading();
+    loading.present();
+
+    this.firebaseService.signIn(user)
+      .then(() => {
+        loading.dismiss();
+
+        // acccion
+        this.utlisService.routerLink('main/home')
+
+      })
+      .catch(err => {
+        loading.dismiss()
+        console.log()
+        this.utlisService.presentToast({ // mensaje de error
+          message: err.message, //firebaseError[err.code],
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+      })
   }
 
 }
--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from '../../../services/firebase.service';
+import { User } from '../../../models/user.model';
+import { UtilsService } from 'src/app/services/utils.service';
+import { firebaseError } from 'src/app/shared/firebaseError';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,11 +11,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  validPattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,20}$"; // minimo 6 y maximo 14
+  
+  form = new FormGroup({
+    uid: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [ Validators.pattern(this.validPattern), Validators.minLength(8)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
 
-  constructor() { }
-
+  firebaseService = inject(FirebaseService)
+  utlisService = inject(UtilsService);
+  
   ngOnInit() {
-    console.log('sign-up')
+  }
+
+  async submit() {
+   console.log('submit')
+    const loading = await this.utlisService.presentLoading();
+    loading.present();
+
+    this.firebaseService.signUp(this.form.value as User)
+      .then(async(res) => {
+        await loading.dismiss();
+
+        // acccion
+        await this.firebaseService.updateUser(this.form.value.name);
+
+        let uid = res.user.uid;
+        this.form.get('uid').setValue(uid);
+
+        await this.setUserInfo(uid); // aqui se hace el registro
+      })
+      .catch(err => {
+        console.log(err.message)  
+        loading.dismiss()
+        this.utlisService.presentToast({ // mensaje de error
+          message: err.message, //firebaseError[err.code],
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+      })
+    }
+
+    async setUserInfo(uid: string) {
+      const loading = await this.utlisService.presentLoading();
+      await loading.present();
+      
+      let path = `users/${uid}`
+      delete this.form.value.password // para que no se guarde 
+      
+      this.firebaseService.setDocument(path, this.form.value).then(async() => {
+        await loading.dismiss();
+        this.utlisService.saveInLocalStorage('user', this.form.value)
+        this.utlisService.routerLink('/main/home');
+        this.form.reset();
+      })
+      .catch(err => {
+        loading.dismiss()
+        this.utlisService.presentToast({ // mensaje de error
+          message: err.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+      })
   }
 
 }
